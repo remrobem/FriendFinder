2736b822243505cda9f11efd4fd705ff098929ac
Rob Martin <rob.martin.5758@gmail.com>
2018-05-16 11:06:26 -0400

    some html, routing and data

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+// list of friends to choose from
+
+let friendsArr = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Billy",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            5,
+            4,
+            1,
+            3,
+            1,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Jane",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            3,
+            1,
+            2,
+            4,
+            1
+        ]
+    }
+];
+
+
+// eport 
+module.exports = friendsArr;
